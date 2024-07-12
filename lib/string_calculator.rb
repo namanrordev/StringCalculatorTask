cea56fb140407b9fd6385c0f9b389b3c438c9ee2
Naman D <dengrenaman@gmail.com>
2024-07-12 12:50:55 +0530

    Add common variable

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,12 +1,16 @@
 class StringCalculator
 
   def self.add(string)
+    # Get delimteter from string
     delimeter = string.split("//").last.split.first if string.start_with?("//")
-    if delimeter
-      string.split("#{delimeter}\n").last.split("#{delimeter}").map(&:to_i).sum
-    else
-      string.split(/delimeter|\n|\,/).map(&:to_i).sum
-    end
-  end
 
+    if delimeter
+      string_arr = string.split("#{delimeter}\n").last.split("#{delimeter}")
+    else
+      string_arr = string.split(/\n|\,/)
+    end
+
+    # Change array of string to array of int and then sum
+    string_arr.map(&:to_i).sum
+  end
 end
